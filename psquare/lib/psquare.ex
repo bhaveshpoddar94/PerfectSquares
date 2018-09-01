2ecf8540484629295601b0885f27c109996e8764
Bhavesh Poddar <poddar.bhavesh94@gmail.com>
2018-09-01 17:08:04 -0400

    #2. Add map method. Use for calling spawn method on individual elements of a list among other things.

--- psquare/lib/psquare.ex
+++ psquare/lib/psquare.ex
@@ -1,18 +1,20 @@
 defmodule PSQUARE do
   @moduledoc """
   Documentation for PSQUARE.
+  contains code for Project 1
   """
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> PSQUARE.hello()
-      :world
-
+  map function
+  takes a list of elements and a function as input
+  applies the function to each element of the list
+  returns the list
   """
-  def hello do
-    :world
+  def map([], _func), do: []
+  def map([head | tail], func) do
+    [func.(head) | map(tail, func)]
   end
+
 end
+
+IO.inspect PSQUARE.sum PSQUARE.map [1,3,2,6,4], fn x -> x*x end
