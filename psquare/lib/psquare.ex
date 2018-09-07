341bdd6dad9f500c2e870c34282496f407c1e63d
Bhavesh Poddar <poddar.bhavesh94@gmail.com>
2018-09-07 17:59:10 -0400

    #1. Add a CLI module to handle command line arguments and spawn tasks.

--- psquare/lib/psquare.ex
+++ psquare/lib/psquare.ex
@@ -1,29 +1,67 @@
-defmodule PSQUARE do
-  @moduledoc """
-  Documentation for PSQUARE.
-  contains code for Project 1
-  """
-
-  @doc """
-  map function
-  takes a list of elements and a function as input
-  applies the function to each element of the list
-  returns the list
-  """
-  def map([], _func), do: []
-  def map([head | tail], func) do
-    [func.(head) | map(tail, func)]
+defmodule PSquare.CLI do
+  def main(args) do
+    case args |> parse_args do
+      {:empty, err} -> IO.puts err
+      {:lt2, err} -> IO.puts err
+      {:gt2, err} -> IO.puts err
+      {:valid, [range, k]} -> spawn_tasks(range, k)
+    end
   end
 
-  @doc """
-  sum function
-  takes a list of integers as input
-  adds the elements of the list
-  returns the sum
-  """
-  def sum([]), do: 0
-  def sum([head | tail]) do 
-    head + sum(tail)
+  defp parse_args(args) do
+    {_, args, _} =  OptionParser.parse(args, switches: [name: :string])
+    l = length(args)
+    case l do
+      l when l == 0 -> {:empty, "Enter values for N and K in that order."}
+      l when l < 2 -> {:lt2, "Enter both N and K values in that order."}
+      l when l > 2 -> {:gt2, "Only two integer values of N and K are accepted in that order."}
+      _ -> {:valid, Enum.map(args, &String.to_integer/1)}
+    end
   end
 
+  defp spawn_tasks(range, k) do
+    Enum.map(1..range, &Task.async(fn -> PerfectSquare.run(&1, &1+k-1) end))
+    |> collect_results
+  end
+  
+  defp collect_results([]), do: IO.puts ""
+  defp collect_results(tasks) do
+    receive do
+      msg ->
+        case Task.find(tasks, msg) do
+          {result, task} ->
+            collect_results(List.delete(tasks, task))
+            if result > 0 do
+              IO.puts result
+            end
+          nil ->
+            collect_results(tasks)
+        end
+    end
+  end
+end
+
+defmodule PerfectSquare do
+  def run(s, e) do
+    if calculate_sum_of_squares(s, e) |> is_perfect_square? do
+      s
+    else
+      -1
+    end
+  end
+
+  def calculate_sum_of_squares(s, e) do
+    sum_util(e) - sum_util(s-1)
+  end
+
+  defp is_perfect_square?(n) when is_integer(n) do
+    :math.sqrt(n)
+    |> :erlang.trunc()
+    |> :math.pow(2) == n
+  end
+
+  def sum_util(n) do
+    numerator = n * (n+1) * (2*n + 1)
+    div(numerator, 6)
+  end
 end
